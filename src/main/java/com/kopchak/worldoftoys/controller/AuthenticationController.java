7881f42c49222def095215b7160d393d0e8c1e15
Iryna <iryna.kopchak39@gmail.com>
2023-09-17 19:54:32 +0300

    feat: add registration functionality to AuthenticationController

---
+++ src/main/java/com/kopchak/worldoftoys/controller/AuthenticationController.java
@@ -0,0 +1,54 @@
+package com.kopchak.worldoftoys.controller;
+
+import com.kopchak.worldoftoys.dto.token.ConfirmTokenDto;
+import com.kopchak.worldoftoys.dto.token.TokenAuthDto;
+import com.kopchak.worldoftoys.dto.user.ResetPasswordDto;
+import com.kopchak.worldoftoys.dto.user.UserAuthDto;
+import com.kopchak.worldoftoys.dto.user.UserRegistrationDto;
+import com.kopchak.worldoftoys.dto.user.UsernameDto;
+import com.kopchak.worldoftoys.exception.AccountIsAlreadyActivatedException;
+import com.kopchak.worldoftoys.exception.IncorrectPasswordException;
+import com.kopchak.worldoftoys.exception.UserNotFoundException;
+import com.kopchak.worldoftoys.exception.UsernameAlreadyExistException;
+import com.kopchak.worldoftoys.model.token.ConfirmationTokenType;
+import com.kopchak.worldoftoys.service.AuthenticationService;
+import com.kopchak.worldoftoys.service.ConfirmationTokenService;
+import com.kopchak.worldoftoys.service.EmailSenderService;
+import com.kopchak.worldoftoys.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@Tag(name = "authentication-controller", description = "Controller for user registration, account confirmation, " +
+        "reset password and login")
+public class AuthenticationController {
+    private final UserService userService;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final EmailSenderService emailSenderService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody UserRegistrationDto userRegistrationDto) {
+        if (userService.isUserRegistered(userRegistrationDto.getEmail())) {
+            throw new UsernameAlreadyExistException(HttpStatus.BAD_REQUEST, "This username already exist!");
+        }
+        String username = userRegistrationDto.getEmail();
+        userService.registerUser(userRegistrationDto);
+        ConfirmTokenDto confirmationToken = confirmationTokenService.createConfirmationToken(username,
+                ConfirmationTokenType.ACTIVATION);
+        emailSenderService.sendEmail(username, userRegistrationDto.getFirstname(),
+                confirmationToken.getToken(), ConfirmationTokenType.ACTIVATION);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
