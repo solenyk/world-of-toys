334fc231e07434b2542f352bfcdc1a18d9081ef1
Iryna <iryna.kopchak39@gmail.com>
2023-09-21 20:29:21 +0300

    feat: add AppAuthenticationEntryPoint

--- src/main/java/com/kopchak/worldoftoys/config/SecurityConfig.java
+++ src/main/java/com/kopchak/worldoftoys/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -20,6 +21,8 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -27,7 +30,7 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v*/auth/**","/swagger-ui/**", "/v3/api-docs/**", "/error")
+                .requestMatchers("/api/v*/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/error")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -40,7 +43,9 @@ public class SecurityConfig {
                 .logout()
                 .logoutUrl("/api/v1/auth/logout")
                 .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+                .and()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
 
         return http.build();
     }
---
+++ src/main/java/com/kopchak/worldoftoys/exception/security/AppAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.kopchak.worldoftoys.exception.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kopchak.worldoftoys.dto.error.ErrorResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AppAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException {
+        HttpStatus httpStatus = HttpStatus.FORBIDDEN;
+        ErrorResponseDto errorResponse = ErrorResponseDto
+                .builder()
+                .error(httpStatus.name())
+                .status(httpStatus.value())
+                .message(ex.getMessage())
+                .build();
+        response.setStatus(httpStatus.value());
+        ObjectMapper mapper = new ObjectMapper();
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        mapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
