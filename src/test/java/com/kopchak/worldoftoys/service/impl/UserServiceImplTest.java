dff8443f2a426765ac3024d833ee4dfbf1c91a08
Iryna <iryna.kopchak39@gmail.com>
2023-09-29 19:53:07 +0300

    test: add tests for isNewPasswordMatchOldPassword and isPasswordsMatch methods of UserServiceImpl

--- src/test/java/com/kopchak/worldoftoys/service/impl/UserServiceImplTest.java
+++ src/test/java/com/kopchak/worldoftoys/service/impl/UserServiceImplTest.java
@@ -1,13 +1,17 @@
 package com.kopchak.worldoftoys.service.impl;
 
 import com.kopchak.worldoftoys.dto.user.UserRegistrationDto;
+import com.kopchak.worldoftoys.exception.InvalidConfirmationTokenException;
 import com.kopchak.worldoftoys.exception.UserNotFoundException;
+import com.kopchak.worldoftoys.model.token.ConfirmationToken;
 import com.kopchak.worldoftoys.model.user.AppUser;
 import com.kopchak.worldoftoys.repository.token.ConfirmTokenRepository;
 import com.kopchak.worldoftoys.repository.user.UserRepository;
+//import org.junit.jupiter.api.AssertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -34,6 +38,9 @@ class UserServiceImplTest {
     private UserServiceImpl userService;
     private String userEmail;
     private AppUser user;
+    private ConfirmationToken confirmationToken;
+    private String confirmToken;
+    private String userPassword;
 
     @BeforeEach
     void setUp() {
@@ -42,6 +49,9 @@ class UserServiceImplTest {
                 .builder()
                 .enabled(true)
                 .build();
+        confirmationToken = ConfirmationToken.builder().user(user).build();
+        confirmToken = "confirm-token";
+        userPassword = "new-password";
     }
 
     @Test
@@ -81,6 +91,54 @@ class UserServiceImplTest {
     public void isUserActivated_NonExistingUser_ThrowsUserNotFoundException() {
         ResponseStatusException exception = assertThrows(UserNotFoundException.class, () ->
                 userService.isUserActivated(userEmail));
+
+        String expectedMessage = "User with this username does not exist!";
+        String actualMessage = exception.getReason();
+        int expectedStatusCode = HttpStatus.NOT_FOUND.value();
+        int actualStatusCode = exception.getStatusCode().value();
+
+        assertEquals(expectedMessage, actualMessage);
+        assertEquals(expectedStatusCode, actualStatusCode);
+    }
+
+    @Test
+    public void isNewPasswordMatchOldPassword_ExistingConfirmTokenAndMatchingPasswords_ReturnsTrue() {
+        when(confirmationTokenRepository.findByToken(confirmToken)).thenReturn(Optional.of(confirmationToken));
+        when(passwordEncoder.matches(userPassword, user.getPassword())).thenReturn(true);
+
+        boolean isNewPasswordMatchOldPassword = userService.isNewPasswordMatchOldPassword(confirmToken, userPassword);
+
+        assertTrue(isNewPasswordMatchOldPassword);
+    }
+
+    @Test
+    public void isNewPasswordMatchOldPassword_NonExistingConfirmToken_ThrowsInvalidConfirmationTokenException() {
+        ResponseStatusException exception = assertThrows(InvalidConfirmationTokenException.class, () ->
+                userService.isNewPasswordMatchOldPassword(confirmToken, userPassword));
+
+        String expectedMessage = "This confirmation token is invalid!";
+        String actualMessage = exception.getReason();
+        int expectedStatusCode = HttpStatus.BAD_REQUEST.value();
+        int actualStatusCode = exception.getStatusCode().value();
+
+        assertEquals(expectedMessage, actualMessage);
+        assertEquals(expectedStatusCode, actualStatusCode);
+    }
+
+    @Test
+    public void isPasswordsMatch_MatchingPasswords_ReturnsTrue() {
+        when(userRepository.findByEmail(userEmail)).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(userPassword, user.getPassword())).thenReturn(true);
+
+        boolean isPasswordsMatch = userService.isPasswordsMatch(userEmail, userPassword);
+
+        assertTrue(isPasswordsMatch);
+    }
+
+    @Test
+    public void isPasswordsMatch_NonExistingUser_ThrowsUserNotFoundException() {
+        ResponseStatusException exception = assertThrows(UserNotFoundException.class, () ->
+                userService.isPasswordsMatch(userEmail, userPassword));
 
         String expectedMessage = "User with this username does not exist!";
         String actualMessage = exception.getReason();
