83b6666a0c64f553812cee4b6d8ce15c2b1e6921
Iryna <iryna.kopchak39@gmail.com>
2023-10-17 19:27:24 +0300

    refactor: refactor the ProductSpecificationsImpl methods using hibernate-jpamodelgen

--- src/main/java/com/kopchak/worldoftoys/repository/product/specifications/impl/ProductSpecificationsImpl.java
+++ src/main/java/com/kopchak/worldoftoys/repository/product/specifications/impl/ProductSpecificationsImpl.java
@@ -1,12 +1,11 @@
 package com.kopchak.worldoftoys.repository.product.specifications.impl;
 
 import com.kopchak.worldoftoys.model.product.Product;
-import com.kopchak.worldoftoys.model.product.category.AgeCategory;
-import com.kopchak.worldoftoys.model.product.category.BrandCategory;
-import com.kopchak.worldoftoys.model.product.category.OriginCategory;
-import com.kopchak.worldoftoys.model.product.category.ProductCategory;
+import com.kopchak.worldoftoys.model.product.Product_;
+import com.kopchak.worldoftoys.model.product.category.*;
 import com.kopchak.worldoftoys.repository.product.specifications.ProductSpecifications;
 import jakarta.persistence.criteria.*;
+import jakarta.persistence.metamodel.SingularAttribute;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
@@ -16,50 +15,53 @@ import java.util.List;
 @Component
 public class ProductSpecificationsImpl implements ProductSpecifications {
     @Override
-    public Specification<Product> hasProductName(String name) {
+    public Specification<Product> hasProductName(String productName) {
         return (root, query, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("name"), "%" + name + "%");
+                criteriaBuilder.like(root.get(Product_.name), "%" + productName + "%");
     }
+
     public Specification<Product> hasPriceLessThanOrEqualTo(BigDecimal maxPrice) {
         return (root, query, criteriaBuilder) ->
-                criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+                criteriaBuilder.lessThanOrEqualTo(root.get(Product_.price), maxPrice);
     }
 
     public Specification<Product> hasPriceGreaterThanOrEqualTo(BigDecimal minPrice) {
         return (root, query, criteriaBuilder) ->
-                criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+                criteriaBuilder.greaterThanOrEqualTo(root.get(Product_.price), minPrice);
     }
+
     public Specification<Product> hasProductInOriginCategory(List<String> originCategories) {
         return hasProductInProductCategory(OriginCategory.class,
-                "originCategory", originCategories);
+                Product_.originCategory, originCategories);
     }
 
     public Specification<Product> hasProductInBrandCategory(List<String> brandCategories) {
         return hasProductInProductCategory(BrandCategory.class,
-                "brandCategory", brandCategories);
+                Product_.brandCategory, brandCategories);
     }
 
     public Specification<Product> hasProductInAgeCategory(List<String> ageCategories) {
-            return (root, query, criteriaBuilder) -> {
-            SetJoin<Product, AgeCategory> productJoin = root.joinSet("ageCategories", JoinType.INNER);
-            Expression<String> ageCategorySlugExpression = productJoin.get("slug");
+        return (root, query, criteriaBuilder) -> {
+            SetJoin<Product, AgeCategory> productAgeCategorySetJoin = root.joinSet(Product_.AGE_CATEGORIES, JoinType.INNER);
+            Expression<String> ageCategorySlugExpression = productAgeCategorySetJoin.get(AgeCategory_.slug);
 
-                Predicate[] predicates = ageCategories.stream()
-                        .map(slug -> criteriaBuilder.equal(ageCategorySlugExpression, slug))
-                        .toArray(Predicate[]::new);
+            Predicate[] predicates = ageCategories.stream()
+                    .map(slug -> criteriaBuilder.equal(ageCategorySlugExpression, slug))
+                    .toArray(Predicate[]::new);
 
-                return criteriaBuilder.or(predicates);
+            return criteriaBuilder.or(predicates);
         };
     }
 
     private Specification<Product> hasProductInProductCategory(Class<? extends ProductCategory> productCategoryType,
-                                                              String joinFieldName,
-                                                              List<String> productCategories) {
+                                                               SingularAttribute<Product, ? extends ProductCategory>
+                                                                       productCategoryAttribute,
+                                                               List<String> productCategories) {
         return (root, query, criteriaBuilder) -> {
-            Join<Product, ?> productJoin = root.join(joinFieldName, JoinType.INNER);
+            Join<Product, ?> productJoin = root.join(productCategoryAttribute, JoinType.INNER);
             Predicate typePredicate = criteriaBuilder.equal(productJoin.type(), productCategoryType);
-            Predicate brandPredicate = productJoin.get("slug").in(productCategories);
-            return criteriaBuilder.and(typePredicate, brandPredicate);
+            Predicate categoryPredicate = productJoin.get(ProductCategory_.SLUG).in(productCategories);
+            return criteriaBuilder.and(typePredicate, categoryPredicate);
         };
     }
 }
