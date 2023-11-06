549bb11cf31aa666924d8704eda450225abb460c
Iryna <iryna.kopchak39@gmail.com>
2023-11-06 18:15:33 +0200

    feat: add insertUserCartItem method to the CartItemRepository

--- src/main/java/com/kopchak/worldoftoys/repository/cart/CartItemRepository.java
+++ src/main/java/com/kopchak/worldoftoys/repository/cart/CartItemRepository.java
@@ -3,7 +3,9 @@ package com.kopchak.worldoftoys.repository.cart;
 import com.kopchak.worldoftoys.dto.cart.CartItemDto;
 import com.kopchak.worldoftoys.model.cart.CartItem;
 import com.kopchak.worldoftoys.model.cart.CartItemId;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,10 +15,26 @@ import java.util.Set;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, CartItemId> {
-    @Query("SELECT new com.kopchak.worldoftoys.dto.cart.CartItemDto(p.name, p.slug, p.price * c.quantity, " +
-            "c.quantity) FROM CartItem c JOIN c.id.product p WHERE c.id.user.email = :email")
+    @Query("SELECT new com.kopchak.worldoftoys.dto.cart.CartItemDto(p.name, p.slug, p.price * c.quantity, c.quantity) " +
+            "FROM CartItem c " +
+            "JOIN c.id.product p " +
+            "WHERE c.id.user.email = :email")
     Set<CartItemDto> findAllCartItemDtosByUserEmail(@Param("email") String email);
 
-    @Query("SELECT SUM(p.price * c.quantity) FROM CartItem c JOIN c.id.product p WHERE c.id.user.email = :email")
+    @Query("SELECT SUM(p.price * c.quantity) " +
+            "FROM CartItem c " +
+            "JOIN c.id.product p " +
+            "WHERE c.id.user.email = :email")
     BigDecimal calculateUserCartTotalByEmail(@Param("email") String email);
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO cart_item (user_id, product_id, quantity) " +
+            "VALUES(" +
+            "(SELECT u.id FROM app_user u WHERE u.email = :userEmail)," +
+            "(SELECT p.id FROM product p WHERE p.slug = :productSlug)," +
+            ":quantity" +
+            ") ON DUPLICATE KEY UPDATE quantity = quantity + :quantity", nativeQuery = true)
+    void insertUserCartItem(@Param("userEmail") String userEmail, @Param("productSlug") String productSlug,
+                            @Param("quantity") Integer quantity);
 }
--- src/main/java/com/kopchak/worldoftoys/service/impl/CartServiceImpl.java
+++ src/main/java/com/kopchak/worldoftoys/service/impl/CartServiceImpl.java
@@ -1,20 +1,11 @@
 package com.kopchak.worldoftoys.service.impl;
 
 import com.kopchak.worldoftoys.dto.cart.AddCartItemDto;
-import com.kopchak.worldoftoys.dto.cart.UserCartDetailsDto;
 import com.kopchak.worldoftoys.dto.cart.CartItemDto;
-import com.kopchak.worldoftoys.exception.ProductNotFoundException;
-import com.kopchak.worldoftoys.exception.UserNotFoundException;
-import com.kopchak.worldoftoys.model.cart.CartItem;
-import com.kopchak.worldoftoys.model.cart.CartItemId;
-import com.kopchak.worldoftoys.model.product.Product;
-import com.kopchak.worldoftoys.model.user.AppUser;
+import com.kopchak.worldoftoys.dto.cart.UserCartDetailsDto;
 import com.kopchak.worldoftoys.repository.cart.CartItemRepository;
-import com.kopchak.worldoftoys.repository.product.ProductRepository;
-import com.kopchak.worldoftoys.repository.user.UserRepository;
 import com.kopchak.worldoftoys.service.CartService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -23,19 +14,12 @@ import java.util.Set;
 @Service
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
-    private final UserRepository userRepository;
-    private final ProductRepository productRepository;
     private final CartItemRepository cartItemRepository;
 
     @Override
     public void addProductToCart(AddCartItemDto addCartItemDto, String email) {
-        AppUser user = userRepository.findByEmail(email).orElseThrow(() ->
-                new UserNotFoundException(HttpStatus.NOT_FOUND, "User with this username does not exist!"));
-        Product product = productRepository.findBySlug(addCartItemDto.slug()).orElseThrow(() ->
-                new ProductNotFoundException(HttpStatus.NOT_FOUND, "Product doesn't exist"));
         int cartItemQuantity = addCartItemDto.quantity() == null ? 1 : addCartItemDto.quantity();
-        CartItem cartItem = new CartItem(new CartItemId(user, product), cartItemQuantity);
-        cartItemRepository.save(cartItem);
+        cartItemRepository.insertUserCartItem(email, addCartItemDto.slug(), cartItemQuantity);
     }
 
     @Override
