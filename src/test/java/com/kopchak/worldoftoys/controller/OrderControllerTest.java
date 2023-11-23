17f0d52f9cd9cb701510cd1d58c0798f13d137b0
Iryna <iryna.kopchak39@gmail.com>
2023-11-23 14:55:40 +0200

    test: add OrderControllerTest and OrderControllerIntegrationTest

---
+++ src/test/java/com/kopchak/worldoftoys/controller/OrderControllerTest.java
@@ -0,0 +1,127 @@
+package com.kopchak.worldoftoys.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kopchak.worldoftoys.config.UserDetailsTestConfig;
+import com.kopchak.worldoftoys.dto.order.AddressDto;
+import com.kopchak.worldoftoys.dto.order.OrderDto;
+import com.kopchak.worldoftoys.dto.order.OrderRecipientDto;
+import com.kopchak.worldoftoys.dto.order.PhoneNumberDto;
+import com.kopchak.worldoftoys.model.user.AppUser;
+import com.kopchak.worldoftoys.service.JwtTokenService;
+import com.kopchak.worldoftoys.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(controllers = OrderController.class)
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+@Import(UserDetailsTestConfig.class)
+class OrderControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private JwtTokenService jwtTokenService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private  OrderRecipientDto orderRecipientDto;
+
+    @BeforeEach
+    void setUp(){
+        orderRecipientDto = OrderRecipientDto
+                .builder()
+                .firstname("Firstname")
+                .lastname("Lastname")
+                .patronymic("Patronymic")
+                .address(new AddressDto("region", "settlement", "street", 1, 1))
+                .phoneNumber(new PhoneNumberDto("95", "1234567"))
+                .build();
+    }
+
+    @Test
+    @WithUserDetails(value = "user@example.com", userDetailsServiceBeanName = "userDetailsService")
+    public void createOrder_AuthUser_ReturnsCreatedStatus() throws Exception {
+        doNothing().when(orderService).createOrder(eq(orderRecipientDto), any(AppUser.class));
+
+        ResultActions response = mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRecipientDto))
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void createOrder_AnonymousUser_ReturnsUnauthorizedStatus() throws Exception {
+        doNothing().when(orderService).createOrder(eq(orderRecipientDto), any(AppUser.class));
+
+        ResultActions response = mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRecipientDto))
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithUserDetails(value = "user@example.com", userDetailsServiceBeanName = "userDetailsService")
+    public void getAllUserOrders_AuthUser_ReturnsOkStatusAndSetOfOrderDto() throws Exception {
+        Set<OrderDto> returnedOrderDtoSet = Set.of(OrderDto.builder().build());
+        when(orderService.getAllUserOrders(any(AppUser.class))).thenReturn(returnedOrderDtoSet);
+
+        ResultActions response = mockMvc.perform(get("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(returnedOrderDtoSet)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void getAllUserOrders_AnonymousUser_ReturnsUnauthorizedStatus() throws Exception {
+        when(orderService.getAllUserOrders(any(AppUser.class))).thenReturn(new HashSet<>());
+
+        ResultActions response = mockMvc.perform(get("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
---
+++ src/test/java/com/kopchak/worldoftoys/integration/OrderControllerIntegrationTest.java
@@ -0,0 +1,153 @@
+package com.kopchak.worldoftoys.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kopchak.worldoftoys.dto.error.ResponseStatusExceptionDto;
+import com.kopchak.worldoftoys.dto.order.*;
+import com.kopchak.worldoftoys.model.order.OrderStatus;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("integrationtest")
+@Transactional
+public class OrderControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private OrderRecipientDto orderRecipientDto;
+    private ResponseStatusExceptionDto accessDeniedErrorResponse;
+    private ResponseStatusExceptionDto unauthorizedErrorResponse;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+        orderRecipientDto = OrderRecipientDto
+                .builder()
+                .firstname("Firstname")
+                .lastname("Lastname")
+                .patronymic("Patronymic")
+                .address(new AddressDto("region", "settlement", "street", 1, 1))
+                .phoneNumber(new PhoneNumberDto("95", "1234567"))
+                .build();
+        accessDeniedErrorResponse = new ResponseStatusExceptionDto(HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.name(), "Access Denied");
+        unauthorizedErrorResponse = new ResponseStatusExceptionDto(HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.name(), "Full authentication is required to access this resource");
+    }
+
+    @Test
+    @WithUserDetails("john.doe@example.com")
+    public void createOrder_AuthUser_ReturnsCreatedStatus() throws Exception {
+        ResultActions response = mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRecipientDto))
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void createOrder_AnonymousUser_ReturnsUnauthorizedStatus() throws Exception {
+        ResultActions response = mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRecipientDto))
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(content().json(objectMapper.writeValueAsString(unauthorizedErrorResponse)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithUserDetails("jane.smith@example.com")
+    public void createOrder_AuthAdminUser_ReturnsForbiddenStatus() throws Exception {
+        ResultActions response = mockMvc.perform(post("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderRecipientDto))
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isForbidden())
+                .andExpect(content().json(objectMapper.writeValueAsString(accessDeniedErrorResponse)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithUserDetails("john.doe@example.com")
+    public void getAllUserOrders_AuthUser_ReturnsOkStatusAndSetOfOrderDto() throws Exception {
+        Set<OrderDto> returnedOrderDtoSet = Set.of(OrderDto
+                .builder()
+                .id("4c980930-16eb-41cd-b998-29d03118d67c")
+                .dateTime(LocalDateTime.of(2023, 11, 19, 18, 9, 52))
+                .orderStatus(OrderStatus.AWAITING_PAYMENT)
+                .products(Set.of(new OrderProductDto("Лялька Клаймбер", BigDecimal.valueOf(1700), 2)))
+                .totalPrice(BigDecimal.valueOf(1700))
+                .build());
+        ResultActions response = mockMvc.perform(get("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(returnedOrderDtoSet)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void getAllUserOrders_AnonymousUser_ReturnsUnauthorizedStatus() throws Exception {
+        ResultActions response = mockMvc.perform(get("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(content().json(objectMapper.writeValueAsString(unauthorizedErrorResponse)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @WithUserDetails("jane.smith@example.com")
+    public void getAllUserOrders_AuthAdminUser_ReturnsForbiddenStatus() throws Exception {
+        ResultActions response = mockMvc.perform(get("/api/v1/order")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()));
+
+        response.andExpect(MockMvcResultMatchers.status().isForbidden())
+                .andExpect(content().json(objectMapper.writeValueAsString(accessDeniedErrorResponse)))
+                .andDo(MockMvcResultHandlers.print());
+    }
+}
