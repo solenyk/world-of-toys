4dad999567c6a2f7d0a559c6a813b19b0183d0ec
Iryna <iryna.kopchak39@gmail.com>
2024-01-09 18:58:46 +0200

    refactor: refactor sendEmail methods of the EmailSenderServiceImpl

--- src/main/java/com/kopchak/worldoftoys/service/impl/EmailSenderServiceImpl.java
+++ src/main/java/com/kopchak/worldoftoys/service/impl/EmailSenderServiceImpl.java
@@ -2,6 +2,13 @@ package com.kopchak.worldoftoys.service.impl;
 
 import com.kopchak.worldoftoys.exception.exception.MessageSendingException;
 import com.kopchak.worldoftoys.exception.exception.UserNotFoundException;
+import com.kopchak.worldoftoys.model.email.EmailType;
+import com.kopchak.worldoftoys.model.email.confirm.ConfirmEmailType;
+import com.kopchak.worldoftoys.model.email.confirm.impl.AccountActivationEmail;
+import com.kopchak.worldoftoys.model.email.confirm.impl.ResetPasswordEmail;
+import com.kopchak.worldoftoys.model.email.status.StatusEmailType;
+import com.kopchak.worldoftoys.model.email.status.impl.OrderStatusEmail;
+import com.kopchak.worldoftoys.model.email.status.impl.PaymentStatusEmail;
 import com.kopchak.worldoftoys.model.order.StatusProvider;
 import com.kopchak.worldoftoys.model.order.payment.PaymentStatus;
 import com.kopchak.worldoftoys.model.token.ConfirmationTokenType;
@@ -10,8 +17,6 @@ import com.kopchak.worldoftoys.repository.user.UserRepository;
 import com.kopchak.worldoftoys.service.EmailSenderService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -26,34 +31,9 @@ import org.thymeleaf.context.Context;
 @Slf4j
 public class EmailSenderServiceImpl implements EmailSenderService {
     private final static String SENDER_EMAIL = "worldoftoys@gmail.com";
-    private final static String ACCOUNT_ACTIVATION_TITLE = "Account activation";
-    private final static String PASSWORD_RESET_TITLE = "Password reset";
-    private final static String PAYMENT_STATUS_TITLE = "Order payment";
-    private final static String ORDER_STATUS_TITLE = "Order status";
-    private final static String ACCOUNT_ACTIVATION_SUBJECT = "Confirm your email";
-    private final static String PASSWORD_RESET_SUBJECT = "Reset your password";
-    private final static String PAYMENT_STATUS_SUBJECT = "Order payment status";
-    private final static String ORDER_STATUS_SUBJECT = "Order status";
-    private final static String ACCOUNT_ACTIVATION_LINK = "/api/v1/auth/confirm?token=";
-    private final static String PASSWORD_RESET_LINK = "/api/v1/auth/reset-password?token=";
-    private final static String LOGIN_LINK = "/api/v1/auth/login";
-    private final static String ACCOUNT_ACTIVATION_LINK_NAME = "Activate Now";
-    private final static String PASSWORD_RESET_LINK_NAME = "Reset password";
-    private final static String PAYMENT_STATUS_LINK_NAME = "Check order payment status";
-    private final static String ORDER_STATUS_LINK_NAME = "Check current order status";
-    private final static String ACCOUNT_ACTIVATION_MSG =
-            "Thank you for registering. Please click on the below link to activate your account. " +
-                    "Link will expire in 15 minutes.";
-    private final static String PASSWORD_RESET_MSG = "Thank you for using our website. " +
-            "Please click on the below link to reset your password. Link will expire in 15 minutes.";
-    private final static String PAYMENT_STATUS_MSG = "Order №%s payment status is %s. For more information, " +
-            "visit our website:";
-    private final static String ORDER_STATUS_MSG = "Order №%s status is %s. For more information, " +
-            "visit our website:";
     private final JavaMailSender mailSender;
     private final UserRepository userRepository;
     private ITemplateEngine templateEngine;
-    private final HttpServletRequest request;
 
     @Override
     @Async
@@ -74,53 +54,43 @@ public class EmailSenderServiceImpl implements EmailSenderService {
     }
 
     @Override
-    public void sendEmail(String userEmail, String confirmToken, ConfirmationTokenType tokenType) throws UserNotFoundException, MessageSendingException {
-        String emailContent;
+    public void sendEmail(String userEmail, String confirmToken, ConfirmationTokenType tokenType)
+            throws UserNotFoundException, MessageSendingException {
         AppUser user = userRepository.findByEmail(userEmail).orElseThrow(() -> {
             log.error("User with username: {} does not exist!", userEmail);
             return new UserNotFoundException(String.format("User with username: %s does not exist!", userEmail));
         });
         String userFirstname = user.getFirstname();
+        ConfirmEmailType confirmEmailType;
         if (tokenType == ConfirmationTokenType.ACTIVATION) {
-            emailContent = buildEmail(ACCOUNT_ACTIVATION_TITLE, userFirstname, ACCOUNT_ACTIVATION_MSG,
-                    getBaseUrl() + ACCOUNT_ACTIVATION_LINK + confirmToken, ACCOUNT_ACTIVATION_LINK_NAME);
-            send(userEmail, emailContent, ACCOUNT_ACTIVATION_SUBJECT);
+            confirmEmailType = new AccountActivationEmail(confirmToken);
         } else {
-            emailContent = buildEmail(PASSWORD_RESET_TITLE, userFirstname, PASSWORD_RESET_MSG,
-                    getBaseUrl() + PASSWORD_RESET_LINK + confirmToken, PASSWORD_RESET_LINK_NAME);
-            send(userEmail, emailContent, PASSWORD_RESET_SUBJECT);
+            confirmEmailType = new ResetPasswordEmail(confirmToken);
         }
+        String emailContent = buildEmail(userFirstname, confirmEmailType);
+        send(userEmail, emailContent, confirmEmailType.getSubject());
     }
 
     @Override
     public <T extends Enum<T> & StatusProvider> void sendEmail(String userEmail, String userFirstname, String orderId,
                                                                T status) throws MessageSendingException {
+        StatusEmailType statusEmailType;
         if (status instanceof PaymentStatus) {
-            String msg = String.format(PAYMENT_STATUS_MSG, orderId, status.getStatus());
-            String emailContent = buildEmail(PAYMENT_STATUS_TITLE, userFirstname, msg, getBaseUrl() + LOGIN_LINK,
-                    PAYMENT_STATUS_LINK_NAME);
-            send(userEmail, emailContent, PAYMENT_STATUS_SUBJECT);
+            statusEmailType = new PaymentStatusEmail(orderId, status);
         } else {
-            String msg = String.format(ORDER_STATUS_MSG, orderId, status.getStatus());
-            String emailContent = buildEmail(ORDER_STATUS_TITLE, userFirstname, msg, getBaseUrl() + LOGIN_LINK,
-                    ORDER_STATUS_LINK_NAME);
-            send(userEmail, emailContent, ORDER_STATUS_SUBJECT);
+            statusEmailType = new OrderStatusEmail(orderId, status);
         }
+        String emailContent = buildEmail(userFirstname, statusEmailType);
+        send(userEmail, emailContent, statusEmailType.getSubject());
     }
 
-    private String buildEmail(String title, String name, String message, String link, String linkName) {
+    private String buildEmail(String name, EmailType emailType) {
         Context context = new Context();
-        context.setVariable("title", title);
+        context.setVariable("title", emailType.getTitle());
         context.setVariable("name", name);
-        context.setVariable("message", message);
-        context.setVariable("link", link);
-        context.setVariable("linkName", linkName);
+        context.setVariable("message", emailType.getMsg());
+        context.setVariable("link", emailType.getLink());
+        context.setVariable("linkName", emailType.getLinkName());
         return templateEngine.process("email", context);
-    }
-
-    private @NotNull String getBaseUrl() {
-        String requestUrl = request.getRequestURL().toString();
-        String servletPath = request.getServletPath();
-        return requestUrl.substring(0, requestUrl.length() - servletPath.length());
     }
 }
